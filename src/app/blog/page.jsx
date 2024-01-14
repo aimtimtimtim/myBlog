0cf7ffb78efa228b64af19f180bc8fd1dc612e4d
Tim <agayevdev@gmail.com>
2024-01-14 13:42:37 +0100

    design

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import styles from './blog.module.css'
 const Blog = () => {
-  return <div>Blog</div>
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Blog</h1>
+      <p className={styles.paragraph}>
+        Here will be posts about frontend development using React and Next.js,
+        and a bit about my journey as a developer, but for now, it's empty.
+      </p>
+    </div>
+  )
 }
 
 export default Blog
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,16 @@
+import MailIcn from '@/src/components/Icons/MailIcn'
+import styles from './contact.module.css'
 import React from 'react'
 
 const Contact = () => {
-  return <div>Contact</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.mail_icn}>
+        <MailIcn />
+      </div>
+      <p className={styles.mail}>agayevdev@gmail.com</p>
+    </div>
+  )
 }
 
 export default Contact
